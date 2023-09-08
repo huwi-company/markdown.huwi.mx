8bd2b1a992e9ea342c978077d15418356ce37741
soknifedev <me@soknife.dev>
2023-09-08 18:33:40 -0300

    require file

--- live.php
+++ live.php
@@ -2,5 +2,8 @@
 header('content-type: text/plain');
 $api = 'https://raw.githubusercontent.com/huwi-company/markdown.huwi.mx/main/';
 $file = $_GET['f'];
+if(empty($file)) {
+    die('File is required');
+}
 
 @readfile($api . $file);
